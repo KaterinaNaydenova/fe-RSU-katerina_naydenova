ab864313908f1bcc72be62b73d7f4cf20573717b
KaterinaNaydenova <katerina.naydenova@mail.ru>
2016-05-06 23:39:12 +0300

    NewContext

--- JS_2.js
+++ JS_2.js
@@ -1,15 +1,20 @@
 function Calculator(leftOperand) {
-	 this.leftOperand = leftOperand;
- this.multiply = function(firstOperand) {
-	 firstResult=leftOperand*firstOperand;
-	 return this;
- }
- 
- this.add = function(secondOperand) {
-	 result=firstResult+secondOperand;
-	 return result;
- }
+	this.leftOperand = leftOperand;
+	
+	this.multiply = function(firstOperand) {
+		leftOperand *= firstOperand;
+		return this;
+	}
+
+	this.add = function(secondOperand) {
+		leftOperand += secondOperand;
+		return this;
+	}
+	
+	this.toString = function() {
+		return leftOperand;
+	}
 }
 
 var calc = new Calculator(12);
-console.log(calc.multiply(2).add(3)); // 27
+alert(calc.multiply(2).add(3).multiply(2)); // 27
