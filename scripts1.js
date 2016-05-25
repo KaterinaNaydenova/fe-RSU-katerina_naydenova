27eac57033d46ee4fc82b47d32cf80b8e29b107f
KaterinaNaydenova <katerina.naydenova@mail.ru>
2016-05-25 21:14:49 +0300

    JQuery

--- scripts1.js
+++ scripts1.js
@@ -1,88 +1,65 @@
-var div1 = document.createElement('div');
-document.body.appendChild(div1);
-
-var s = {
+	$('body').append('<div class="div1">');
+	$(".div1").css({"position": "absolute", 
+	"width": "98.2%", 
+	"height": "25%", 
+	"background-color": "rgb(160,223,209)", 
+	"border": "2px solid rgb(106,173,158)", 
+	"border-radius": "10px"});
 	
-	  width: '98,2%',
-	  height: '200px',
-	  backgroundColor: '#76EEC6',
-	  border: '2px solid rgb(106,173,158)',
-	  borderRadius: '10px',
-}
- 
-for(var key in s){
-  div1.style[key] = s[key];
-}
 
-
-var div2 = document.createElement('div');
-div1.appendChild(div2);
-var d = {
-	position: 'relative',
-	height: '91%',
-	width: '98.65%',
-	top: '4%',
-	left: '0.5%',
-	bottom: '4%',
-	fontFamily: 'sans-serif',
-	border: '2px solid rgb(53,195,174)',
-	backgroundColor: 'white',
-			}
-for(var key in d){
-    div2.style[key] = d[key];
-}
-
-var div3 = document.createElement('div');
-div2.appendChild(div3);
-var	f = {
-
-	margintop: '0px',
-	left: '0px',
-	right: '0px',
-
-	height: '23%',
-	backgroundColor: '#FFD700',
-	paddingLeft: '2%',
-	paddingTop: '0.4%',
-	fontSize: '25px',
-	}
+	$('.div1').append('<div class="div2">');
+	$(".div2").css({"position": "relative", 
+	"width": "98.6%", 
+	"height": "91%", 
+	"top":"4%", 
+	"left":"0.5%", 
+	"bottom":"4%", 
+	"font-family":"sans-serif", 
+	"background-color": "white", 
+	"border": "2px solid rgb(53,195,174)"});
 	
-for(var key in f){
-    div3.style[key] = f[key];
-}
-div3.innerHTML = 'Header';
+	
+	$('.div2').append('<div class="div3">');
+	$('.div3').css({"position": "relative", 
+	"height": "23%", 
+	"top":"0px", 
+	"left":"0px", 
+	"right":"0px", 
+	"background-color": "rgb(255,201,0)", 
+	"padding-left":"2%", 
+	"padding-top":"0.4%",
+	"font-size":"4vh"});
+	
 
-var div4 = document.createElement('div');
-div2.appendChild(div4);
+	$('.div2').append('<p class="other"');	
+	$('.other').css({"position": "absolute",
+		"top":"30%",
+		"left":"2%",
+		"bottom":"0",
+		"height":"auto",
+		"overflow": "auto"});
+			
+		
+	$('.div2').append('<span class="figure">');
+	$('.figure').css({
+		"position": "absolute",
+		"top":"25%",
+		"left":"2%",
+		"border":"20px solid transparent", 
+		"border-top-color":"rgb(255,201,0)"
+	});
+		
+		
+	
+	
+	
+		
 
-var g = {
 
-	width: '0px',
-	marginLeft: '2%',
-	border: '20px solid transparent', 
-	borderTopColor: '#FFD700',
-}
-for(var key in g){
-    div4.style[key] = g[key];
-}
-
-var div5 = document.createElement('div');
-div2.appendChild(div5);
+			
 
 
 
-var p = document.createElement('p');
-div5.appendChild(p);
 
-var j = {
-	marginLeft: '2%',
-	marginTop: '0%',
-	overflow: 'auto',
-}
 
-for(var key in j){
-   p.style[key] = j[key];
-}
-
-p.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."; 
-
+	
--- scripts2.js
+++ scripts2.js
@@ -1,111 +1,64 @@
-var ul = document.createElement('ul');
-document.body.appendChild(ul);
- 
-var a = {
-	display: 'block',
-	position: 'fixed',
-	left: '0',
-	marginTop: '-141px',
-	listStyle: 'none',
-	fontSize: '20px',
-	color: 'white',
-	cursor: 'pointer',
-}
+	$('body').append('<ul>');
+	$('ul').css({'cursor': 'pointer',
+	'color': 'white',
+	'fontSize': '15px',
+	'marginTop': '-97px',
+	'left': '0',
+	'display': 'block',
+	'position': 'fixed',})
 
-for(var key in a){
-  ul.style[key] = a[key];
-}
+	$('ul').append('<li class="li1">');
+	$('ul').append('<li class="li2">');
+	$('ul').append('<li class="li3">');
+	$('ul').append('<li class="li4">');
+	$('ul').append('<li class="li5">');
+	$('ul').append('<li class="TopMenu">');
 
-var li1 = document.createElement('li');
-ul.appendChild(li1);
-var li2 = document.createElement('li');
-ul.appendChild(li2);
-var li3 = document.createElement('li');
-ul.appendChild(li3);
-var li4 = document.createElement('li');
-ul.appendChild(li4);
-var li5 = document.createElement('li');
-ul.appendChild(li5);
-var li6 = document.createElement('li');
-ul.appendChild(li6);
-
-var s = {
-	width: '125px',
-	height: '25px',
-	color: 'white',
-	backgroundColor: '#35baf6',
-	border: '1px solid #0cacf4',
-	textAlign: 'center',
-	textDecoration: 'none',
-	fontSize: '20px',	  
-}
-
-for(var key in s){
-  li1.style[key] = s[key];
-  li2.style[key] = s[key];
-  li3.style[key] = s[key];
-  li4.style[key] = s[key];
-  li5.style[key] = s[key];
-  li6.style[key] = s[key];
-}
-
-li6.style.borderRadius = '0 0 15px 15px';
-li6.id = 'topmenu';
-
-li1.id = 'menu1';
-li2.id = 'menu2';
-li3.id = 'menu3';
-li4.id = 'menu4';
-li5.id = 'menu5';
-
-li1.innerHTML = 'Menu 1';
-li2.innerHTML = 'Menu 2';
-li3.innerHTML = 'Menu 3';
-li4.innerHTML = 'Menu 4';
-li5.innerHTML = 'Menu 5';
-li6.innerHTML = 'Top menu';
-
-
-function mouseOverMenuN() {
-	this.style.backgroundColor = '#03a9f4'; 
-}
-
-function mouseOutMenuN() {
-	this.style.backgroundColor = '#35baf6'; 
-}
-
-document.getElementById('menu1').addEventListener('mouseover', mouseOverMenuN);
-document.getElementById('menu2').addEventListener('mouseover', mouseOverMenuN);
-document.getElementById('menu3').addEventListener('mouseover', mouseOverMenuN);
-document.getElementById('menu4').addEventListener('mouseover', mouseOverMenuN);
-document.getElementById('menu5').addEventListener('mouseover', mouseOverMenuN);
-
-document.getElementById('menu1').addEventListener('mouseout', mouseOutMenuN);
-document.getElementById('menu2').addEventListener('mouseout', mouseOutMenuN);
-document.getElementById('menu3').addEventListener('mouseout', mouseOutMenuN);
-document.getElementById('menu4').addEventListener('mouseout', mouseOutMenuN);
-document.getElementById('menu5').addEventListener('mouseout', mouseOutMenuN);
-
-
-var topmenu = document.getElementById('topmenu');
-
-function mouseOver() {
 	
-	this.style.boxShadow = "-5px 3px 5px #a3dffb, 5px 3px 5px #a3dffb";	
-}
+$('.li1').text('Menu1');
+$('.li2').text('Menu2');
+$('.li3').text('Menu3');
+$('.li4').text('Menu4');
+$('.li5').text('Menu5');
+$('.TopMenu').text('TopMenu');
 
-function mouseOut() {
+$('li').css({
+	'width': '125px',
+	'height': '16px',
+	'background-color': '#35baf6',
+	'border': '1px solid #0cacf4',
+	'text-align': 'center',
+	
+})
 
-	this.style.boxShadow = "-5px 3px 5px transparent, 5px 3px 5px transparent";
-}
-topmenu.addEventListener('mouseover', mouseOver);
-topmenu.addEventListener('mouseout', mouseOut);
+$('.TopMenu').css({'border-radius': '0 0 5px 5px'})
 
-function mouseClick() {
-	ul.style.marginTop = "-7px";	
-}	
+$('.TopMenu').on('click',function(){
+$('ul').css({'marginTop': '-10px'})
+});	
 
-topmenu.addEventListener('click', mouseClick);
+$('li').on('mouseover', function(){
+	$(this).css({'background-color': '#0cacf4'})
+});
+
+$('li').on('mouseleave', function(){
+	$('li').css({'background-color': '#35baf6'})
+});
+
+$('.TopMenu').on('mouseover', function(){
+$('.TopMenu').css({'box-shadow': '-5px 3px 5px #a3dffb'})
+});	
+
+$('.TopMenu').on('mouseleave', function(){
+$('.TopMenu').css({'box-shadow': 'none'})
+});	
+
+
+
+
+
+
+
 
 
 
